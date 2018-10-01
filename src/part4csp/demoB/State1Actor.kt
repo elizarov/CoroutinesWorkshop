2b3407b12aebe854e233767bdce2d5e93b47e9c0
Roman Elizarov <elizarov@gmail.com>
2018-10-01 15:23:14 +0300

    Fixes and additions to part4

--- src/part4csp/demoB/State1Actor.kt
+++ src/part4csp/demoB/State1Actor.kt
@@ -7,7 +7,7 @@ sealed class CounterMsg
 object IncCounter : CounterMsg() // one-way message to increment counter
 class GetCounter(val response: CompletableDeferred<Int>) : CounterMsg() // a request with reply
 
-fun counterActor() = actor<CounterMsg> {
+fun CoroutineScope.counterActor() = actor<CounterMsg> {
     var counter = 0 // actor state
     for (msg in channel) { // iterate over incoming messages
         when (msg) {
